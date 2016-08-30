9cc355fdd0ef76e0e1e38d1311dc9905beeadb3d
Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
2016-08-30 15:13:06 +0800

    add sale_order_date
    that allow to edit the sale order date in quotation
    
    Signed-off-by: Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>

---
+++ sale_order_date/__init__.py
@@ -0,0 +1,10 @@
+##############################################################################
+#
+#    Copyright (C) 2016 JCrosoft Limited
+#    Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
+#
+#    Licence: AGPLv3
+#
+##############################################################################
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ sale_order_date/__openerp__.py
@@ -0,0 +1,26 @@
+##############################################################################
+#
+#    Copyright (C) 2016 JCrosoft Limited
+#    Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>
+#
+#    Licence: AGPLv3
+#
+##############################################################################
+{
+    'name': 'sale order date',
+    'version': '9.0.1.0',
+    'author' : 'JCrosoft',
+    'license' : 'AGPL-3',
+    'contributors' : ['Jean-Christophe PLAGNIOL-VILLARD <plagnioj@jcrosoft.com>'],
+    'category': 'Sales',
+    'description': """
+    """,
+    'website': 'http:s//www.jcrosoft.com',
+    'depends': ['sale'],
+    'data': [
+        'views/sale.xml',
+     ],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
